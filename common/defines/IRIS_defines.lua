19b62ba2d9b5bda548db3053713e423f2537eb90
Rekordny <loadingwheel@gmail.com>
2023-08-27 10:16:12 -0300

    initialization

---
+++ common/defines/IRIS_defines.lua
@@ -0,0 +1,11 @@
+NDefines.NGame.START_DATE = "3090.1.1.12"
+NDefines.NGame.END_DATE = "3103.1.1.1"
+
+
+NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "3090.1.1.12"
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 200							-- Max opinion value cap.
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -200						-- Min opinion value cap.
+
+NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NCountry.MAX_WAR_SUPPORT = 1.0					
+
