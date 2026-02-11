4342014b1b58df8f91c50ef40cabe64784a2bac2
Katyusha233 <anran20030913@163.com>
2026-02-11 11:20:41 +0800

    早樱

--- events/事件代码生成.py
+++ events/事件代码生成.py
@@ -1,14 +1,14 @@
 def generate_hoi4_event_code(event_number):
     return f"""country_event = {{#
-    id = USF_parties.{event_number}
-    title = USF_parties.{event_number}.t
-    desc = USF_parties.{event_number}.desc
-    #picture = GFX_event_USF_parties_{event_number}
+    id = SKR_flavor.{event_number}
+    title = SKR_flavor.{event_number}.t
+    desc = SKR_flavor.{event_number}.desc
+    #picture = GFX_event_SKR_flavor_{event_number}
     is_triggered_only = yes
     fire_only_once = yes
 
     option = {{
-        name = USF_parties.{event_number}.a
+        name = SKR_flavor.{event_number}.a
 
         ai_chance = {{
             base = 10
@@ -17,9 +17,9 @@ def generate_hoi4_event_code(event_number):
 }}"""
 #文本自己改
 def generate_localization(event_number):
-    return f""" USF_parties.{event_number}.t: ""
- USF_parties.{event_number}.desc: ""
- USF_parties.{event_number}.a: ""
+    return f""" SKR_flavor.{event_number}.t: ""
+ SKR_flavor.{event_number}.desc: ""
+ SKR_flavor.{event_number}.a: ""
 """
 #文本自己改
 def generate_content(mode, min_event_num, max_event_num):
@@ -60,9 +60,9 @@ def main():
         return
     
     if mode == "1":
-        output_filename = f"USF_parties_events_{min_event}_to_{max_event}.txt"
+        output_filename = f"SKR_flavor_events_{min_event}_to_{max_event}.txt"
     else:
-        output_filename = f"USF_parties_localization_{min_event}_to_{max_event}.yml"
+        output_filename = f"SKR_flavor_localization_{min_event}_to_{max_event}.yml"
     
     with open(output_filename, "w", encoding="utf-8") as file:
         file.write(content)
