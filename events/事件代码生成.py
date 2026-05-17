62c03befedb0efb9d83ffc9ba8697416127d9f4b
Katyusha233 <anran20030913@163.com>
2026-05-17 21:08:36 +0800

    修修补补

--- events/事件代码生成.py
+++ events/事件代码生成.py
@@ -1,14 +1,14 @@
 def generate_hoi4_event_code(event_number):
     return f"""country_event = {{#
-    id = SKR_ending.{event_number}
-    title = SKR_ending.{event_number}.t
-    desc = SKR_ending.{event_number}.desc
-    #picture = GFX_event_SKR_ending_{event_number}
+    id = SKR.{event_number}
+    title = SKR.{event_number}.t
+    desc = SKR.{event_number}.desc
+    #picture = GFX_event_SKR_{event_number}
     is_triggered_only = yes
     fire_only_once = yes
 
     option = {{
-        name = SKR_ending.{event_number}.a
+        name = SKR.{event_number}.a
 
         ai_chance = {{
             base = 10
@@ -17,11 +17,34 @@ def generate_hoi4_event_code(event_number):
 }}"""
 #文本自己改
 def generate_localization(event_number):
-    return f""" SKR_ending.{event_number}.t: ""
- SKR_ending.{event_number}.desc: ""
- SKR_ending.{event_number}.a: ""
+    return f""" SKR.{event_number}.t: ""
+ SKR.{event_number}.desc: ""
+ SKR.{event_number}.a: ""
 """
 #文本自己改
+def generate_hoi4_news_event_code(event_number):
+    return f"""news_event = {{#
+    id = SKR_news.{event_number}
+    title = SKR_news.{event_number}.t
+    desc = SKR_news.{event_number}.desc
+    #picture = GFX_SKR_news_{event_number}
+    is_triggered_only = yes
+	major = yes
+
+    option = {{
+        name = SKR_news.{event_number}.a
+
+        ai_chance = {{
+            base = 10
+        }}
+    }}
+}}"""
+#文本自己改
+def generate_news_event_localization(event_number):
+    return f""" SKR_news.{event_number}.t: ""
+ SKR_news.{event_number}.desc: ""
+ SKR_news.{event_number}.a: ""
+"""
 def generate_content(mode, min_event_num, max_event_num):
     try:
         max_event_num = int(max_event_num)
@@ -36,6 +59,10 @@ def generate_content(mode, min_event_num, max_event_num):
                 content += generate_hoi4_event_code(i) + "\n\n"
             elif mode == "2":
                 content += generate_localization(i)
+            elif mode == "3":
+                content += generate_hoi4_news_event_code(i) + "\n\n"
+            elif mode == "4":
+                content += generate_news_event_localization(i)
         
         return content
     except ValueError:
@@ -46,9 +73,11 @@ def main():
     print("请选择生成模式：")
     print("1 - 生成事件代码")
     print("2 - 生成事件本地化")
-    mode = input("请输入模式编号(1/2): ")
+    print("3 - 生成新闻事件代码")
+    print("4 - 生成新闻事件本地化")
+    mode = input("请输入模式编号(1/2/3/4): ")
     
-    if mode not in ["1", "2"]:
+    if mode not in ["1", "2", "3", "4"]:
         print("无效的模式选择！")
         return
     
@@ -60,9 +89,11 @@ def main():
         return
     
     if mode == "1":
-        output_filename = f"SKR_ending_events_{min_event}_to_{max_event}.txt"
+        output_filename = f"SKR_events_{min_event}_to_{max_event}.txt"
+    elif mode == "3":
+        output_filename = f"SKR_news_events_{min_event}_to_{max_event}.txt"
     else:
-        output_filename = f"SKR_ending_localization_{min_event}_to_{max_event}.yml"
+        output_filename = f"SKR_localization_{min_event}_to_{max_event}.yml"
     
     with open(output_filename, "w", encoding="utf-8") as file:
         file.write(content)
